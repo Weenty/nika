c78206f081d186780c26b9a1943ed681cc92675a
Weenty <flaky12r@mail.ru>
2022-03-22 16:15:03 +0500

    migrate

--- main/models.py
+++ main/models.py
@@ -1,2 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class users(AbstractUser):
+    phone_number = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.username
+
+class section(models.Model):
+    name = models.CharField(max_length=45)
+
+class caterogy(models.Model):
+    name = models.CharField(max_length=45)
+    section = models.ManyToManyField(section)
+    def __str__(self):
+        return self.name
+
+class catalogy(models.Model):
+    name = models.CharField(max_length=45)
+    discription = models.TextField(default='')
+    cost = models.DecimalField(max_digits=7, decimal_places=0)
+    discount = models.IntegerField(default=0)
+    img = models.ImageField(upload_to=f'uploads/{name}/')
+    quantity = models.IntegerField(default=0)
+    purpose = models.CharField(max_length=255, default='')
+    brand = models.CharField(max_length=45, default='')
+    procer = models.CharField(max_length=45, default='')
+    best_before_date = models.DateField(default='')
+    composition = models.CharField(max_length=255, default='')
+    # package = здесь как-то нужно выбдвинуть разновидности упаковок     
+    rating = models.FloatField(default=0)
+    number_of_views = models.IntegerField(default=0)
+    category = models.ManyToManyField(caterogy)
+    def __str__(self):
+        return self.name
 
--- nika/settings.py
+++ nika/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
+
+AUTH_USER_MODEL = 'main.users'
 
 USE_I18N = True
 
