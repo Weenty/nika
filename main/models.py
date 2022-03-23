1ce141310bc17560e46f08a01571b109018da565
Weenty <flaky12r@mail.ru>
2022-03-23 14:09:38 +0500

    register

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
+from enum import unique
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class users(AbstractUser):
     phone_number = models.CharField(max_length=45)
+    email = models.EmailField(unique=True)
     def __str__(self):
         return self.username
 
--- nika/settings.py
+++ nika/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'main',
+    'djoser',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
